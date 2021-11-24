e87097d0f4e191dacadb101c77afd867cf64d08b
AXEL IVÁN REYES ORELLANA <axel.reyes@sansano.usm.cl>
2021-11-24 18:13:44 -0300

    DMoN, Map and README in PyPI v0.0.9

--- SpatialCluster/__init__.py
+++ SpatialCluster/__init__.py
@@ -4,5 +4,5 @@ Init file for the SpatialCluster library.
 
 from SpatialCluster.core import DMoN_Clustering, visualize_map_sample
 
-version_info = (0, 0, 8)
+version_info = (0, 0, 9)
 __version__ = ".".join([str(x) for x in version_info])
--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.8'
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '0.0.9'
 DESCRIPTION = 'Spatial cluster package'
 
 # Setting up
@@ -12,10 +17,13 @@ setup(
     author="AxelReyesO (Axel Reyes O)",
     author_email="<axel.reyes@sansano.usm.cl>",
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=['tensorflow','scipy','numpy','matplotlib','folium'],
     url="https://github.com/AxlKings/SpatialCluster",
     keywords='python spatial urban cluster',
+    license='MIT',
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
