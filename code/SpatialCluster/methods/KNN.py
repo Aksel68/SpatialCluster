1bfe98aa2a94d59be24366f204e93f7ee35f3020
AxlKings <axel.reyes@sansano.usm.cl>
2023-06-10 19:04:49 -0400

    KNN optimized

--- code/SpatialCluster/methods/KNN.py
+++ code/SpatialCluster/methods/KNN.py
@@ -5,9 +5,11 @@ from scipy import spatial
 import pandas as pd
 import numpy as np
 
-def KNN_Clustering(features_X, features_position, attribute, threshold, location, condition=">", k=5, K=30, alfa = 0.01, leafsize=10):
+def KNN_Clustering(features_X, features_position, attribute, threshold, location, condition=">", k=1500, K=5000, alfa = 0.01, leafsize=10):
     features_X = data_format(features_X)
     features_position = position_data_format(features_position)
+    if(k > K):
+        raise Exception('k has to be greater than K')
     features_X = pd.concat([features_position, features_X], axis=1)
     t = []
     f = []
@@ -28,42 +30,39 @@ def KNN_Clustering(features_X, features_position, attribute, threshold, location
             f.append(df[df[attribute] == threshold].shape[0])
         x_centroid.append((df["lon"].mean(), df["lat"].mean()))
     tree = spatial.KDTree(data = x_centroid, leafsize = leafsize)
-    Nk = tree.query(x_centroid, k = k)[1]
-    NK = tree.query(x_centroid, k = K)[1]
+    NK = tree.query(x_centroid, k = len(x_centroid))[1]
 
     Tk = []
     Fk = []
-    for j in range(Nk.shape[0]):
-        ti = 0
-        fi = 0
-        for i in Nk[j]:
-            ti += t[i]
-            fi += f[i]
-        Tk.append(ti)
-        Fk.append(fi)
     TK = []
     FK = []
     for j in range(NK.shape[0]):
         ti = 0
         fi = 0
+        Ti = 0
+        Fi = 0
         for i in NK[j]:
-            ti += t[i]
-            fi += f[i]
-        TK.append(ti)
-        FK.append(fi)
-
+            if ti < k:
+                ti += t[i]
+                fi += f[i]
+            if Ti < K:
+                Ti += t[i]
+                Fi += f[i]
+            else:
+                break
+        Tk.append(ti)
+        Fk.append(fi)
+        TK.append(Ti)
+        FK.append(Fi)
     p_hot = []
     p_cold = []
-    for j in range(len(x_centroid)): 
+    for j in range(len(x_centroid)):
         hpd = hypergeom(TK[j], FK[j], Tk[j])
-        p = 0
-        for z in range(Fk[j], Tk[j]):
-            p += hpd.pmf(z)
-        p_hot.append(p)
-        p = 0
-        for z in range(Fk[j]):
-            p += hpd.pmf(z)
-        p_cold.append(p)
+        a1 = hpd.cdf(Fk[j])
+        b1 = hpd.cdf(Tk[j])
+        p_hot.append(b1-a1)
+        b = hpd.cdf(Fk[j])
+        p_cold.append(b)
 
     centroid_clusters = {}
     for i in range(len(x_centroid)):
