034c4957fec44487a0e006eca6cea6be328c9b5c
AxlKings <axel.reyes@sansano.usm.cl>
2023-06-25 19:36:51 -0400

    fix data format

--- code/SpatialCluster/__init__.py
+++ code/SpatialCluster/__init__.py
@@ -2,7 +2,7 @@
 Init file for the SpatialCluster library.
 """
 
-version_info = (0, 0, 76)
+version_info = (0, 0, 77)
 __version__ = ".".join([str(x) for x in version_info])
 
 __all__ = [
--- code/SpatialCluster/preprocess/data_format.py
+++ code/SpatialCluster/preprocess/data_format.py
@@ -4,7 +4,7 @@ def attributes_format(df):
     attr_cols = []
     for col in cols:
         if(col != "lon" and col != "lat"):
-            if("float" in str(type(df[col][0]))):
+            if("float" in str(type(df[col].values[0])) or "int" in str(type(df[col].values[0]))):
                 attr_cols.append(col)
     attributes = df[attr_cols]
     return position, attributes
@@ -15,7 +15,7 @@ def attributes_with_zone_format(df, zona):
     attr_cols = []
     for col in cols:
         if(col != "lon" and col != "lat"):    
-            if("float" in str(type(df[col][0]))):
+            if("float" in str(type(df[col].values[0])) or "int" in str(type(df[col].values[0]))):
                 attr_cols.append(col)
             elif(col == zona):
                 attr_cols.append(col)
